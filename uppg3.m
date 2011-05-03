32ee9670034eaf0f49ff7dfb2b8ad54906ed9acc
m4rw3r <m4rw3r@gmail.com>
2011-05-03 08:55:02 +0200

    More cleaning

--- uppg3.m
+++ uppg3.m
@@ -9,52 +9,44 @@ function uppg3a()
 	% particles for last calculation
 	N = 24;
 	%number of eigen oscillations plotted
-	num_plotted=6;
+	num_plotted = 6;
 	
 	%set titles and labels for figure 1
 	title([num2str(num_plotted), ' f\"{o}rsta egensv\"{a}ningarna som st\aa{}ende v\aa{}gor f\"{o}r ', num2str(s), ' till ', num2str(N), ' partiklar'], 'interpreter', 'latex');
 	xlabel('Avst\aa{}nd fr\aa{}n v\"{a}nsterv\"{a}ggen', 'interpreter', 'latex');
 	ylabel('Olika antal partiklar samt deras amplituder');
 	
-	for i=s:N
+	for i = s:N
 		[t, y] = calcStandingWaves(i);
 		
 		% normalize maxumum amplitude for lowest frequency, so they will be easy to compare
-		middle = y(1:(i+2), floor((i+2)/2));
+		middle = y(1:(i + 2), floor((i + 2) / 2));
 		y = (y / max(middle)) * 0.9;
 		
 		% ensure that we dont try to plot more eigen oscillation than we have
-		to_plot=min(i,num_plotted);
+		to_plot = min(i,num_plotted);
 		
-		% plot to_plot first eigenoscillation
-		% plot in different subplot for different number of particles
-		subplot(N-s+1,1,i+1-s)
-		% sets label for each subplot
+		subplot(N - s + 1, 1 , i + 1 - s)
 		hold on
-		ylabel([num2str(i), ' par.']);
-		% makes every x-tick visible
-		set(gca,'XTick',1:N)
 		
-		plot(t(:,1:to_plot), y(:,1:to_plot),'-*');
+		ylabel([num2str(i), ' par.']);
+		set(gca,'XTick',1:N)
+		plot(t(:, 1:to_plot), y(:, 1:to_plot), '-*');
 	end
 	
-	% sets common xlabel
-	xlabel('Partikel index')
+	xlabel('Partikel-index')
 	
 	hold off
 	figure(2)
 	
 	hold on;
 	
-	%first eigen oscillation we want to plot
-	f = 1;
-	%last eigen oscillation we want to plot
-	l = num_plotted;
+	first = 1;
+	last  = num_plotted;
 	
-	% sets labels and title
-	xlabel('tid, $\frac{2\pi}{\omega_o}$', 'interpreter','latex');
+	xlabel('tid, $\frac{2\pi}{\omega_o}$', 'interpreter', 'latex');
 	ylabel('index f\"{o}r egenfrekvens samt normaliserad amplitud' ,'interpreter','latex');
-	title(['egenfrekvenser ', num2str(f), ' till ', num2str(l) ,' f\"{o}r antal partiklar ', num2str(s), ' till ', num2str(N), ' i olika f\"{a}rger'], 'interpreter','latex');
+	title(['egenfrekvenser ', num2str(first), ' till ', num2str(last) ,' f\"{o}r antal partiklar ', num2str(s), ' till ', num2str(N), ' i olika f\"{a}rger'], 'interpreter','latex');
 	
 	% to plot differenet frequencies with different colors.
 	colors = ['b', 'r', 'g', 'k', 'c', 'y', 'm'];
@@ -62,18 +54,17 @@ function uppg3a()
 	% plots eigen oscillations
 	for num_particles = s:N
 		% to plot different numbers of particles in different subplots
-		subplot(N-s+1,1,num_particles+1-s)
+		subplot(N - s + 1, 1, num_particles + 1 - s)
 		ylabel([num2str(num_particles), ' par.']);
 		hold on
 		
-		for i = f:l
-			% to get index for color to use
-			colorindex=mod(i-l,length(colors))+1;
+		for i = first:last
+			colorindex = mod(i - last, length(colors)) + 1;
 			
 			% sets ticks 
-			set(gca,'XTick',0:10)
+			set(gca, 'XTick', 0:10)
 			[t, y] = calcFrequencies(num_particles, i);
-			plot(t,y,colors(colorindex));
+			plot(t, y, colors(colorindex));
 		end
 	end
 	
@@ -85,19 +76,13 @@ function [t, y] = calcStandingWaves(num_particles)
 	
 	[eigvec, ~] = eig(A);
 	
-	% create a list of arrays whose positions correspond to a node in the oscillations
-	% 0 and num_particles+1 are endpoints
-	t = 0:num_particles+1;
-	% repeat for num_particles so we have them correspond to the number of eigenvectors
-	% and transpose to make them ready for plot
+	t = 0:num_particles + 1;
 	t = repmat(t, num_particles, 1)';
 	
-	% first node, 0
 	y(1, 1:num_particles) = zeros(1, num_particles);
 	% each eigenvector contains amplitudes for every node in a specific resonance oscillation,
 	% add them so plot connects their amplitudes and connect them to the end nodes
 	y(2:(num_particles+1), 1:num_particles) = eigvec;
-	% last node, 0
 	y((num_particles + 2), 1:num_particles) = zeros(1, num_particles);
 	
 function [m] = triDiag(side_length)
@@ -112,8 +97,7 @@ function [m] = triDiag(side_length)
 function [t, y] = calcFrequencies(num_particles, vector_index)
 	K     = triDiag(num_particles);
 	[P D] = eig(K);
-	%2*pi to plot in unit t = omega_o / (2 * pi)
+	% 2*pi to plot in unit t = omega_o / (2 * pi)
 	freq  = sqrt(D(vector_index, vector_index)) * 2 * pi;
 	t     = linspace(0, 10);
 	y     = sin(freq .* t);
-
--- uppg4.m
+++ uppg4.m
@@ -1,108 +1,95 @@
 function apa()
-clf
-figure(1)
-clf
-	%generera tridiagonala matrisen
+	clf
+	figure(1)
+	clf
+	
+	% Generera tridiagonala matrisen
 	A = triDiag(100);
 	
-	% homogenledet
+	% Homogenledet
 	noll = zeros(100,1);
-	% hastighetsledet
-	v = zeros(100,1);
-	%ger mitterta partiklarna en hastighet vid t=0
+	% Hastighetsledet
+	v    = zeros(100,1);
+	% ger mitterta partiklarna en hastighet vid t = 0
 	v(45:55,1) = ones(11,1);
-	v = 0.1 * v;
+	v          = 0.1 * v;
+	
+	%__________
+	% Vi har ekvationerna:
+	% P C .* sin(fi) = noll                        (1)
+	% P sqrt(lambda) * omega0 .* C .* cos(Fi) = v  (2)
+	% (1) => sin(fi) = 0 (ty C != 0) => fi = pi*n
+	% (2) ger:
+	% P * C .* cos(Fi) = v ./ (sqrt(lambda) * omega0)
+	% C .* cos(Fi) = P \ (v ./ (sqrt(lambda) * omega0))
+	% Varpå C fås från insättning av Fi, från (1), och Fi fås från C.
+	%___________
 	
 	rhs(1:100, 1) = noll;
 	rhs(1:100, 2) = v;
 	
-	%__________
-	%Vi har ekvationerna:
-	%A C.*sin(fi)=noll                      (1)
-	%A sqrt(lambda)*omega0.*C.*cos(Fi)=v    (2)
-	%(1) ger:
-	%=>  fi=arcsin((A\noll)/C)=arcsin(a/C)
-	%<=> arccos(sqrt(C.^2+a.^2)/C)
-	%(2) ger:
-	%A*C.*cos(Fi)=v./(sqrt(lambda)*omega0)
-	%C.*cos(Fi)=A\(v./(sqrt(lambda)*omega0))
-	%Varpå C fås från insättning av Fi, från (1), och Fi fås från C.
-	%___________
-	
 	[P D]  = eig(A);
 	lambda = D * ones(100, 1);
 	
-	%skriver högerleden som en konstanter
 	solution = P \ rhs;
 	
-	a = solution(:, 1);
-	b = solution(:, 2);
-	
-	%vi låter omega0 vara tidsenhet, (fås hastighet i enhet meter*omega0)
-	k = sqrt(lambda); %*omega0
-	C = b./k;
-	%C = sqrt(c.^2+a.^2);
-	fi = 0;%fi = asin(a./C); %blir 0 alltid, ty Aa=0 saknar trivial lösning, enl. IMT; dock: ±n*pi
+	% Vi låter omega0 vara tidsenhet, (fås hastighet i enhet meter*omega0)
+	k = sqrt(lambda); % * omega0
+	C = solution(:, 2) ./ k;
+	fi = 0;
 	
 	result = @(t) [P * (C .* sin(sqrt(lambda) .* t + fi))];
 	
-	time  = 200;
-	stime = 0;
-	
-	num_steps = time*10;
+	time      = 200;
+	stime     = 0;
+	num_steps = time * 10;
 	time_step = time / num_steps;
-   %{
-	for t=0:num_steps-1
-		data(1:100, t+1) = result(t*time_step + stime);
-		
-		h=plot(linspace(0, 100),result(t*time_step + stime));
-		ylim([-1 1]);
-        
-		%saveas(h, strcat('plot', sprintf('%d', t), '.png'));
-       pause(0.005);
-        
-    end
-    %}
-    %få noll vid vägg 2? data(1:101,num_steps+1)=zeros(101,1);
-    
-    %plotta 3d-plot med tiden som y axel vid diskreta tidpunkter
-    %{
-    hold on
-    xlabel(['Partikel index']);
-    ylabel(['$\frac{1}{\omega_o}$'], 'interpreter','latex');
-    h_ylabel = get(gca,'YLabel');
-set(h_ylabel,'FontSize',20); 
-    zlabel(['Amplitud']);
-    
-    for t=0:time/100:time
-        ett=ones(1,100);
-        plot3(linspace(0, 100),t*ett,result(t*time_step + stime));
-        zlim([-1 1]);
-    end
-    %}
-    
-    figure(2)
-    
-    for i = 1:100
-        if sum(P(1, i)) < 0
-            P(:, i) = -P(:, i);
-        end
-    end
-    
-    maxamp = P .* repmat(abs(C), 1, 100);
-    hold on
-    plot(repmat(1:100, 100, 1)', maxamp')
-    
-    
-	%plot(repmat(linspace(stime, stime+time, num_steps), 100, 1)', data)
 	
-	%t = linspace(0,10); %kommer behöva ändras för andra storlekar på A, 100 element just nu
-	%tplot = repmat(t,100,1); %repmat för dimension
-	%result(t')
-	%plot(tplot',result(t')); %t transponat för behöver kolonn
-	%plotten ska tolkas hur? ser konstigt ut?
+	for t = 0:(num_steps - 1)
+		data(1:100, t + 1) = result(t * time_step + stime);
+		
+		h = plot(linspace(0, 100), result(t * time_step + stime));
+		ylim([-1 1]);
+		
+		%saveas(h, strcat('plot', sprintf('%d', t), '.png'));
+		pause(0.005);
+	end
+	%}
+	% Få noll vid vägg 2? data(1:101, num_steps + 1) = zeros(101, 1);
+	
+	% Plotta 3d-plot med tiden som y axel vid diskreta tidpunkter
+	%{
+	clf
+	hold on
+	xlabel(['Partikel-index']);
+	ylabel(['$\frac{1}{\omega_o}$'], 'interpreter', 'latex');
+	h_ylabel = get(gca, 'YLabel');
+	set(h_ylabel, 'FontSize', 20); 
+	zlabel(['Amplitud']);
+	
+	for t = 0:time/100:time
+		ett = ones(1,100);
+		plot3(linspace(0, 100), t * ett, result(t * time_step + stime));
+		zlim([-1 1]);
+	end
+	%}
+	
+	figure(2)
+	
+	% Force the first eigen-oscillation to be positive,
+	% matlab might not give them all the same sign
+	for i = 1:100
+		if sum(P(1, i)) > 0
+			P(:, i) = -P(:, i);
+		end
+	end
+	
+	maxamp = P .* repmat(C, 1, 100);
+	hold on
+	plot(repmat(1:100, 100, 1)', maxamp')
+	
 function [matrix] = triDiag(side_length)
-	%Generera den tridiagonala matrisen:
+	% Generera den tridiagonala matrisen:
 	n = -ones(side_length - 1, 1);
 	B = diag(n, 1);
 	C = diag(n, -1);
